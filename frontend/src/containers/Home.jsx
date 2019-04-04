ee780f336dcbea51e9d33cc5299777b14644d2ac
richiello <richiello@hotmail.com>
2019-04-04 22:56:59 +0200

    Add startpage styling

--- frontend/src/containers/Home.jsx
+++ frontend/src/containers/Home.jsx
@@ -4,50 +4,86 @@ import Box from '../components/Box'
 import AutoComplete from '../components/AutoComplete';
 import styled from "styled-components"
 
+const Title = styled.h1`
+    font-size: 70px;
+    color: #6C9F9B;
+    font-weight: bold;
+`;
+
 const Button = styled.button`
-    width: 176px;
-    height: 40px;
-    border-radius: 10px;
-    outline: none;
-    background-color: blue;
-    font-size: 16px;
-    text-align: center;
     color: #fff;
+    background-color: #6C9F9B;
     border: none;
+    border-radius: 10px;
+    font-size: 16px;
+    height: 80px;
+    margin: 20px;
+    outline: none;
+    text-transform: uppercase;
+    text-align: center;
+    transition: background-color 0.5s ease;
+    width: 200px;
+
+    :focus {
+        outline: none;
+    }
+
+    :hover {
+        background-color: #4a7a76;
+    }
 `;
 
 const ButtonContainer = styled.div`
-  display: 'inline-block';
-  height: '3rem';
-  justify-content: 'space-between';
+    display: inline-block;
+    height: 3rem;
+    justify-content: space-between;
 `;
 
-const Container = styled.div`
-  height: '10rem';
-  display: 'flex';
-  width: '10rem';
+const Box = styled.div`
+    width: 600px;
+    height: 350px;
+    top: -10%;
+    margin: auto;
+    position: relative;
+    text-align: center;
+    padding: 30px;
+`;
+
+const BodyWrapper = styled.section`
+    height: 100%;
+    margin: auto;
+    display: flex;
+`;
+
+const SubTitle = styled.p`
+    font-weight: bold;
+    font-size: 30px;
+    padding: 10px 0;
+    color: #6C9F9B;
 `;
 
 export default class Home extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
 
-  render() {
+    render() {
 
-    return (
-      <Fragment>
-        <Navigation />
-        <Container>
-          <h1> nDoors </h1>
-          <p> welcome </p>
-          <ButtonContainer>
-            <Button> Add Reference </Button>
-            <Button> Logga in </Button>
-          </ButtonContainer>
-        </Container>
-      </Fragment >
-    )
-  }
+        return (
+            <Fragment>
+                <Navigation />
+                <BodyWrapper>
+                    <Box>
+                        <Title> nDoors </Title>
+                        <SubTitle> welcome </SubTitle>
+                        <ButtonContainer>
+                            <Button> Add Reference </Button>
+                            <Button> Logga in </Button>
+                        </ButtonContainer>
+                    </Box>
+                </BodyWrapper>
+            </Fragment >
+        )
+    }
 }

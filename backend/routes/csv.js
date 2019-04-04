a2654618a89e13f026efac63d6407b08fb8931d3
Per Sonberg <per.sonberg@LAN-MAC106.local>
2019-04-04 22:54:59 +0200

    Refactor backend for csv

--- backend/routes/csv.js
+++ backend/routes/csv.js
@@ -1,60 +1,25 @@
-const routes = require('express').Router();
-const fs = require('fs');
-const parse = require('csv-parse')
+const routes = require('express').Router()
+const loadCsv = require('../csv-provider');
 
 let skills = [];
 let abilities = [];
 
 routes.get('/abilities', async (req, res) => {
-    res.send(abilities.filter(search(req.query.q)));
+    const result = abilities.filter(search(req.query.q || '')).slice(0, 10);
+    res.send(result);
 });
 
 
 routes.get('/skills', async (req, res) => {
-    res.send(skills.filter(search(req.query.q)));
+    const result = skills.filter(search(req.query.q || '')).slice(0, 10);
+    res.send(result);
 });
 
 
-const loadSkillsAsync = async () => {
-    const file = await readFile('./static/skills.csv');
-    const parsed = await parseFile(file)
-    skills = parsed.map(parseLine(0))
-
-}
-
-const loadAbilitiesAsync = async () => {
-    const file = await readFile('./static/abilities.csv')
-    const parsed = await parseFile(file)
-    abilities = parsed.map(parseLine(0))
-}
-
-// Load file from disk
-const readFile = url => new Promise((resolve, reject) => {
-    fs.readFile(url, 'utf8', function (err, contents) {
-        if (err) return reject(err);
-        resolve(contents);
-    });
-});
-
-// Parse csv
-const parseFile = file => new Promise((resolve, reject) => {
-    parse(file, {
-        comment: '#'
-    }, function (err, output) {
-        if (err) return reject(err);
-        resolve(output);
-    })
-})
-
-// Parse line
-const parseLine = index => str => str[0].split(';')[index];
-
+loadCsv('./static/abilities.csv').then(res => abilities = res);
+loadCsv('./static/skills.csv').then(res => skills = res);
 
 // Search for string
-const search = query => str => str.indexOf(query) > -1;
-
-// Load files
-loadSkillsAsync();
-loadAbilitiesAsync();
+const search = query => str => str && str.indexOf(query) > -1;
 
 module.exports = routes;

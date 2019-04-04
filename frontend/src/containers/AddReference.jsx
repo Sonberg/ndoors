af254eb5b3008423dfab93afe3dfa171ff1e195f
John Stjernrup <john.stjernrup@valtech.com>
2019-04-04 22:46:27 +0200

    addReference added

---
+++ frontend/src/containers/AddReference.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import Box from '../components/Box'
+
+export default class AddReferences extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      referenceState: 1
+    }
+  }
+  onContinue() {
+    this.setState({ referenceState: this.state.referenceState + 1 })
+  }
+  render() {
+    let referencePage
+    switch (this.state.referenceState) {
+      case 1:
+        referencePage = (
+          <div>
+            <Box>ads</Box>
+            <button onClick={() => this.onContinue()}>step 1</button>
+          </div>
+        )
+        break
+      case 2:
+        referencePage = (
+          <button onClick={() => this.onContinue()}>step 2</button>
+        )
+        break
+      default:
+        referencePage = <h1>END</h1>
+    }
+
+    return <div>{referencePage}</div>
+  }
+}

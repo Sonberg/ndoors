0c3121e55e7e67c47296aa99210f56c6426f257f
Per Sonberg <per.sonberg@LAN-MAC106.local>
2019-04-04 20:22:52 +0200

    Skills endpoint added

--- backend/controllers/resourceController.js
+++ backend/controllers/resourceController.js
@@ -1,21 +1,21 @@
-const db = require('./../config/index');
+const firestore = require('./../config/index');
 
 module.exports = function (collection) {
     return {
         all: async function (req, res) {
-            let reference = db.collection(collection);
+            let reference = firestore.collection(collection);
 
             for (const key in req.query) {
                 reference = reference.where(key, '==', req.query[key]);
             }
 
             const snap = await reference.get()
-            const data = snap.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+            const data = snap.docs.map(doc => ({ id: doc.id, ...doc.data() }));
 
             res.json(data);
         },
         get: async function (req, res) {
-            const snap = await db.collection(collection).doc(req.params.id).get();
+            const snap = await firestore.collection(collection).doc(req.params.id).get();
             const data = snap.data();
 
             res.json({
@@ -24,22 +24,22 @@ module.exports = function (collection) {
             });
         },
         post: async function (req, res) {
-            await db.collection(collection).add(req.body);
+            await firestore.collection(collection).add(req.body);
             res.status(200).send();
         },
         put: async function (req, res) {
-            await db.collection(collection).doc(req.params.id).set(req.body);
+            await firestore.collection(collection).doc(req.params.id).set(req.body);
             res.status(200).send();
         },
         patch: async function (req, res) {
-            const snap = db.collection(collection).doc(req.params.id).get();
+            const snap = firestore.collection(collection).doc(req.params.id).get();
             const data = { ...snap.data(), ...req.body }
 
-            db.collection(collection).doc(req.params.id).set(data);
+            firestore.collection(collection).doc(req.params.id).set(data);
             res.status(200).send();
         },
         delete: async function (req, res) {
-            await db.collection(collection).doc(req.params.id).delete();
+            await firestore.collection(collection).doc(req.params.id).delete();
             res.status(200).send();
         }
     }
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,5 +3,6 @@ const routes = require('express').Router();
 // Map api routes
 routes.use('/api/references/', require('./references'));
 routes.use('/api/users/', require('./users'));
+routes.use('/api/skills/', require('./skills'));
 
 module.exports = routes;

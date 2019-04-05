144cfa5a74a1dfd630b759b4d66fbfa3017dd9bb
John Stjernrup <john.stjernrup@valtech.com>
2019-04-05 13:58:13 +0200

    backend files

--- backend/routes/csv.js
+++ backend/routes/csv.js
@@ -1,25 +1,29 @@
 const routes = require('express').Router()
-const loadCsv = require('../csv-provider');
+const loadCsv = require('../csv-provider')
 
-let skills = [];
-let abilities = [];
+let skills = []
+let abilities = []
+let professions = []
 
 routes.get('/abilities', async (req, res) => {
-    const result = abilities.filter(search(req.query.q || '')).slice(0, 10);
-    res.send(result);
-});
-
+  const result = abilities.filter(search(req.query.q || '')).slice(0, 10)
+  res.send(result)
+})
 
 routes.get('/skills', async (req, res) => {
-    const result = skills.filter(search(req.query.q || '')).slice(0, 10);
-    res.send(result);
-});
+  const result = skills.filter(search(req.query.q || '')).slice(0, 10)
+  res.send(result)
+})
+routes.get('/professions', async (req, res) => {
+  const result = professions.filter(search(req.query.q || '')).slice(0, 10)
+  res.send(result)
+})
 
-
-loadCsv('./static/abilities.csv').then(res => abilities = res);
-loadCsv('./static/skills.csv').then(res => skills = res);
+loadCsv('./static/abilities.csv').then(res => (abilities = res))
+loadCsv('./static/skills.csv').then(res => (skills = res))
+loadCsv('./static/professions.csv').then(res => (professions = res))
 
 // Search for string
-const search = query => str => str && str.indexOf(query) > -1;
+const search = query => str => str && str.indexOf(query) > -1
 
-module.exports = routes;
+module.exports = routes

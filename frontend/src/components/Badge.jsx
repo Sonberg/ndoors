9e6ad51d0d957d8d3e146ff661fdf101c670bb0a
Per Sonberg <per.sonberg@LAN-MAC106.local>
2019-04-05 10:50:58 +0200

    style refactor

--- frontend/src/components/Badge.jsx
+++ frontend/src/components/Badge.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default ({ children }) => (
-    <div children={children} className="z-depth-1" style={{ background: 'rgb(255,152,101)', padding: '0.25em .5em', color: 'white' }} />
+    <div children={children} style={{ background: 'rgb(255,152,101)', padding: '0.25em .5em', color: 'white' }} />
 );

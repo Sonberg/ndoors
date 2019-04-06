f8ba6fdfc7c47ec74bd45d04675828fb6dda303a
Per Sonberg <per.sonberg@LAN-MAC106.local>
2019-04-06 09:38:09 +0200

    logga ut button removed

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -41,7 +41,7 @@ class Navigation extends Component {
                   {localStorage.getItem('loggedIn') ? (
                     <Links />
                   ) : null}
-                  <li><a href="/" onClick={this.logOut}>{localStorage.getItem('user') || "Logga ut"}</a></li>
+                  <li><a href="/" onClick={this.logOut}>{localStorage.getItem('user')}</a></li>
                 </ul>
               </div>
             </div>

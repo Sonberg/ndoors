2a2128ba0a1ddbfc94fa43383d6332357c1f12c4
“dyrhagi” <maxdyrhage@gmail.com>
2019-05-17 12:56:42 +0200

    add bootstrap to overview

--- src/routes/Overview/index.jsx
+++ src/routes/Overview/index.jsx
@@ -8,6 +8,8 @@ import { bindActionCreators } from 'redux'
 import { Link } from 'react-router-dom'
 import OverviewReference from './components/Overview.Reference';
 import { actionCreators } from '../../store/References';
+import { Container, Row, Col } from 'react-bootstrap';
+
 
 const sectionHeader = {
     fontWeight: '600',
@@ -24,38 +26,26 @@ class Overview extends Component {
         const { references } = this.props;
 
         return (
-            <div className="container" style={{ width: '80%' }}>
-                <div className="row" >
-                    <div className="col s12" style={{ marginBottom: '2em' }}>
-                        <OverviewUser />
-                    </div>
-                    <div className="col s7 " style={{ paddingRight: '2em', borderRight: '1px solid rgb(222, 222, 222)', }}>
-
-                        <div style={{ padding: '1em 0', display: 'flex', justifyContent: 'space-between' }}>
-                            {references.length === 0 ?
-                                <Link to="/add-reference" style={{ width: '100%' }} ><Button children="Share references" />
-                                </Link>
-                                : <Link to="/shared-references" style={{ width: '100%' }} ><Button children="Share references" />
-                                </Link>
-                            }
-                            <Button as="a" href="/add-reference" style={{ width: '50%' }}> Add references</Button>
-                        </div>
-
+            <Container style={{ width: '80%' }}>
+                <Row>
+                    <Col sm='12' children={<OverviewUser />} />
+                    <Col sm='7'>
+                        {renderLinks(references)}
                         <h5 children="Requested references" style={sectionHeader} />
                         {references.map((x, index) => <OverviewReference key={index} {...x} />)}
 
-                    </div>
-                    <div className="col s5 " style={{ paddingLeft: '1.5em' }}>
-                        <OverviewSkills />
-                        <div style={{ marginLeft: '0.75em' }}>
-                            <OverviewSuggestions />
-                        </div>
-                    </div>
-                </div>
-            </div>
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
+const renderLinks = (references) => (
+    <div className="py-3 d-flex justify-content-between">
+        {references.length ? <Link to="/shared-references"><Button children="Share references" /> </Link> : null}
+        <Button as="a" href="/add-reference"> Add references</Button>
+    </div>
+)
 
 export default connect(
     state => ({

4f9c2e5d17c9b6b67bda834f2a94d6d69e835630
“dyrhagi” <maxdyrhage@gmail.com>
2019-07-19 15:52:53 +0200

    added buttons to add references

--- src/routes/AddReference2/components/Step.jsx
+++ src/routes/AddReference2/components/Step.jsx
@@ -1,17 +1,38 @@
 import React from 'react'
-import { Row, Col } from 'react-bootstrap'
+import { Row, Col, Button } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-export default ({ index, currentIndex, setIndex, title, children }) => {
+
+
+export default ({ index, currentIndex, setIndex, total, title, children }) => {
     const shouldDisplayContent = index === currentIndex;
+    const shouldDisablePrev = index === 0;
+    const shouldDisableNext = index + 1 == total;
     const onClick = () => setIndex(index);
+    const onClickPrev = () => {
+        if (shouldDisablePrev){
+            return 
+        }
+        setIndex(index - 1)};
+    const onClickNext = () => {
+        if (shouldDisableNext){
+           return
+        }
+        setIndex(index + 1)
+    };
     const Content = () => (
         <Row className="border-top">
             <Col className="p-3">
                 {children}
             </Col>
-            <Col sm="auto" className="d-flex align-items-center">
-                pil
-        </Col>
+            <Col sm="auto" className="p-3">
+                <Row className={`mb-3 ${shouldDisablePrev ? "disabled-button" : null}`}>
+                    <FontAwesomeIcon icon="chevron-circle-up" size="2x" onClick={onClickPrev} /> 
+                </Row>
+                <Row className={`${shouldDisableNext ? "disabled-button" : null}`}>
+                    <FontAwesomeIcon icon="chevron-circle-down" size="2x" onClick={onClickNext}  /> 
+                </Row>
+            </Col>
         </Row>
     );
 
@@ -20,7 +41,7 @@ export default ({ index, currentIndex, setIndex, title, children }) => {
             <Col className="shadow-sm">
                 <Row>
                     <Col className="p-3" onClick={onClick}>
-                        {title}
+                        {title} 
                     </Col>
                 </Row>
                     {shouldDisplayContent ? <Content /> : null}
--- src/routes/AddReference2/index.jsx
+++ src/routes/AddReference2/index.jsx
@@ -3,13 +3,16 @@ import { Container, Row, Col } from 'react-bootstrap'
 import Step from './components/Step';
 
 export default () => {
-    const steps = [Step, Step, Step, Step, Step, Step, Step, Step, Step, Step];
-    const Test = [{title: 'Titel', children: 'Text'}]
+    const Steps = 
+    [{title: 'Who is your reference?', children: 'Text'}, 
+    {title: 'Describe your relation', children: "testtext 2"},
+    {title: 'List your competencies', children: "testtext 3"},
+    {title: 'Leave a personal note', children: "testtext 3"}]
     const [currentIndex, setIndex] = useState(0);
 
     return (
         <Container className="mt-5">
-            {Test.map((props, index) => <Step index={index} currentIndex={currentIndex} setIndex={setIndex} {...props} />)}
+            {Steps.map((props, index) => <Step index={index} currentIndex={currentIndex} setIndex={setIndex} total={Steps.length} {...props} />)}
         </Container>
     )
 }

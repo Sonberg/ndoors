7fbe1f99b090a0c2552306e8e557bb97b8c892ab
Per <per.sonberg@gmail.com>
2019-07-22 08:25:15 +0200

    mock new overview

--- src/routes/Overview/components/Overview.User.jsx
+++ src/routes/Overview/components/Overview.User.jsx
@@ -7,22 +7,29 @@ export default () => {
     const { user } = useAuth();
 
     return (
-        <Row className="align-items-center mt-5 pb-3 border-bottom">
-            <Col>
-                <h1 children={user && user.name} />
-            </Col>
-            <Col sm="7">
-                <Row>
-                    <Col>
-                        <OverviewUserStats title="References verified" value="84%" />
-                    </Col>
-                    <Col>
-                        <OverviewUserStats title="Unique views" value="47%" />
-                    </Col>
-                    <Col>
-                        <OverviewUserStats title="People I've referenced" value="2" />
-                    </Col>
-                </Row>
-            </Col>
-        </Row>);
+        <>
+            <Row className="align-items-center mt-5 pb-3">
+                <Col>
+                    <h1 children={user && user.name} />
+                </Col>
+                <Col sm="7">
+                    <Row>
+                        <Col>
+                            <OverviewUserStats title="References verified" value="84%" />
+                        </Col>
+                        <Col>
+                            <OverviewUserStats title="Unique views" value="47%" />
+                        </Col>
+                        <Col>
+                            <OverviewUserStats title="People I've referenced" value="2" />
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <div className="border-bottom" />
+                </Col>
+            </Row>
+        </>);
 }
--- src/routes/Overview/index.jsx
+++ src/routes/Overview/index.jsx
@@ -1,65 +1,82 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import OverviewUser from './components/Overview.User'
-import Button from '../../components/Button';
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import OverviewReference from './components/Overview.Reference';
 import { actionCreators } from '../../store/References';
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-class Overview extends Component {
+const Overview = ({ loadCreated }) => {
 
-    componentDidMount() {
-        this.props.loadCreated();
-    }
+    useEffect(() => { loadCreated(); })
 
-    render() {
-        const { references } = this.props;
-
-        return (
-            <Container style={{ width: '80%' }}>
-                <Row>
-                    <Col>
-                        <OverviewUser />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col className="my-4">
-                        <Button to="/add-reference" className="mr-4">Request reference</Button>
-                        <Button to="/add-reference">Submit reference</Button>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Card className="shadow-sm">
-                            <Card.Body>
-                                <span className="h1" children="16" />
-                                <small children="verified references" />
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card className="shadow-sm">
-                            <Card.Body>
-                                <span className="h1" children="16" />
-                                <small children="pending references" />
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card className="shadow-sm">
-                            <Card.Body>
-                                <span className="h1" children="16" />
-                                <small children="sent references" />
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-        )
-    }
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <OverviewUser />
+                </Col>
+            </Row>
+            <Row>
+                <Col sm="12" lg="8">
+                    <Row className="my-4">
+                        <Col>
+                            <div className="rounded p-4 h5 mb-0 bg-secondary-light">
+                                <FontAwesomeIcon icon="plus-circle" className="mr-2" /> Request reference
+                            </div>
+                        </Col>
+                        <Col>
+                            <div className="rounded p-4 h5 mb-0 bg-secondary-light">
+                                <FontAwesomeIcon icon="plus-circle" className="mr-2" /> Submit reference
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row className="mb-5">
+                        <Tab value="26" color="#ffbab3" label="verified references" />
+                        <Tab value="16" label="pending references" />
+                        <Tab value="6" label="sent references" />
+                    </Row>
+                    <TabContent />
+                    <TabContent />
+                    <TabContent />
+                    <TabContent />
+                    <TabContent />
+                    <TabContent />
+                    <TabContent />
+                    <TabContent />
+                </Col>
+                <Col lg="4">
+                </Col>
+            </Row>
+        </Container>
+    )
 }
 
+const Tab = ({ value, label, color }) => (
+    <Col>
+        <div className={`text-center shadow-sm py-4 rounded`} style={{ border: `1px solid ${color ? color : 'none'}` }}>
+            <span className="h1" children={value} />
+            <div>
+                <small children={label} />
+            </div>
+        </div>
+    </Col>
+);
+
+const TabContent = ({ }) => (
+    <Row className="">
+        <Col>
+            <div className="rounded shadow-sm p-4 mb-3 d-flex">
+                <Col className="h6 mb-0">
+                    Hanna Lundell
+                </Col>
+                <Col>CEO</Col>
+                <Col>Google</Col>
+                <Col sm="auto">Icon</Col>
+            </div>
+        </Col>
+    </Row>
+);
 
 export default connect(
     state => ({
